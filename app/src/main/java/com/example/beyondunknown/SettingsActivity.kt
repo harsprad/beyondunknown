2389e2b052717e64d1079c98c033a0293e73533b
BattyBoi69 <101750118+BattyBoi69@users.noreply.github.com>
2024-06-19 18:40:33 +0100

    Merge remote changes with unrelated histories

--- app/src/main/java/com/example/beyondunknown/SettingsActivity.kt
+++ app/src/main/java/com/example/beyondunknown/SettingsActivity.kt
@@ -6,7 +6,7 @@ import com.example.beyondunknown.databinding.ActivitySettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var binding: ActivitySettingsBinding   
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
